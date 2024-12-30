89f0fef8d3186c7c768aa4506397d29f1649e70e
Sarraju Varma Sagi <varmasagi18@gmail.com>
2024-12-30 14:14:25 +0530

    first commit

--- lua/themes/dark.lua
+++
@@ -1,1 +0,0 @@
-return { dir = './local/dark_theme.lua', priority = 1000, config = function() require 'themes.local.dark_theme' end }
--- lua/themes/new_dark.lua
+++
@@ -1,5 +0,0 @@
-return {
-	priority = 1000,
-	dir = './local/new_dark_theme.lua',
-	config = function() require 'themes.local.new_dark_theme' end,
-}
